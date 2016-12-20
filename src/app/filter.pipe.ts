225cea3119931a4e80df3524b1291378e34a1f6d
Santino Petrovic <santinopetrovic@gmail.com>
2016-12-20 14:19:39 +0100

    Added filter pipe, ngfor will not make duplicated div elements.

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe ({
+	name: "filter",
+	pure: false
+})
+
+@Injectable()
+
+export class FilterPipe implements PipeTransform {
+    transform(value, productID) {
+        // filter items array, items which match and return true will be kept, false will be filtered out
+        return value.filter(player => player.id == productID);
+    }
+}
